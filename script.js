49f457bf649144707e4447912d8fa03d9cf56b91
Amit17214 <120364136+Amit17214@users.noreply.github.com>
2023-10-09 21:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,26 @@
-//your code here
- function sortBandNames(bandNames) {
-            // Define articles to be excluded
-            const articles = ['a', 'an', 'the'];
+// Define the array of band names
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'Led Zeppelin', 'The Who', 'Pink Floyd'];
 
-	 // Sort the band names without articles
-            const sortedNames = bandNames.sort((a, b) => {
-                const nameA = a.toLowerCase();
-                const nameB = b.toLowerCase();
+// Function to remove articles ('a', 'an', 'the') from a band name
+function removeArticles(name) {
+  return name.replace(/^(a|an|the)\s+/i, '');
+}
 
-				
-                // Helper function to remove articles
-                const removeArticles = name => {
-                    for (const article of articles) {
-                        if (name.startsWith(article + ' ')) {
-                            return name.slice(article.length + 1); // +1 to remove the space
-                        }
-                    }
-                    return name;
-                };
+// Sort the band names without articles in lexicographic order
+bandNames.sort((a, b) => {
+  const nameA = removeArticles(a).toLowerCase();
+  const nameB = removeArticles(b).toLowerCase();
+  if (nameA < nameB) return -1;
+  if (nameA > nameB) return 1;
+  return 0;
+});
 
+// Get the <ul> element by its id 'bands'
+const ulElement = document.getElementById('bands');
 
-				// Compare names without articles
-                const compareA = removeArticles(nameA);
-                const compareB = removeArticles(nameB);
-
-                return compareA.localeCompare(compareB);
-            });
-
-            return sortedNames;
-        }
-
-		// Array of band names
-        let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Aerosmith'];
-
-        // Sort the band names without articles
-        const sortedBandNames = sortBandNames(bandNames);
-
-        // Get the 'ul' element by its id
-        const bandList = document.getElementById('band');
-
-        // Add sorted band names to the 'ul' element
-        for (const name of sortedBandNames) {
-            const listItem = document.createElement('li');
-            listItem.textContent = name;
-            bandList.appendChild(listItem);
-        }
+// Create and append <li> elements for each sorted band name
+bandNames.forEach((name) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
